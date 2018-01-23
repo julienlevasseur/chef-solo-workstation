1197e6144bdf949f3d8375ffa13775a8917862a1
Julien Levasseur <levasju1@LAP-MTL-LEVASJU.vasco.com>
2018-01-23 18:16:05 -0500

    add attributes.json, solo.rb & run.sh

---
+++ run.sh
@@ -0,0 +1,82 @@
+#!/usr/bin/env bash
+
+GREEN='\033[32m'
+RED='\033[91m'
+NC='\033[0m'
+
+ARG=$1
+PWD=`pwd`
+LOGFILE='/tmp/chef_client_why_run.log'
+
+set_ownership() {
+	sudo chown -R $USER. .
+}
+
+berks_vendor() {
+	sudo berks vendor
+	set_ownership
+}
+
+why_run() {
+	#berks_vendor
+	sudo -H chef exec chef-solo -W -c $PWD/solo.rb -j $PWD/attributes.json|sudo tee $LOGFILE
+}
+
+chef_run() {
+	#berks_vendor
+	sudo chef exec chef-solo -c $PWD/solo.rb -j $PWD/attributes.json
+}
+
+exec_test() {
+	chef exec inspec exec cookbooks/workstation/test/smoke/default/
+	chef exec inspec exec cookbooks/sublime_text/test/smoke/default/
+}
+
+help() {
+	echo "Usage: run.sh [--help] [--vendor] [--whyrun] [--test]"
+	echo " "
+	echo "--help 		| -h	 Print this message."
+	echo "--vendor 	| -V	 Vendoring dependencies."
+	echo "--whyrun 	| -W	 Execute a Why Run."
+	echo "--test 		| -t	 Execute test suite."
+}
+
+
+if [[ $ARG == '--help' || $ARG == '-h' ]]
+then
+	help
+elif [[ $ARG == '--vendor' || $ARG == '-V' ]]
+then
+	printf "${GREEN}[INFO] Cleaning berks-cookbooks folder${NC}\n"
+	printf "${GREEN}[INFO] Cleaning Berksfile.lock file${NC}\n"
+	rm Berksfile.lock
+	rm -rf berks-cookbooks/*
+	printf "${GREEN}[INFO] Vendoring cookbooks${NC}\n"
+	berks_vendor
+elif [[ $ARG == '--whyrun' || $ARG == '-W' ]]
+then
+	printf "${GREEN}[INFO] Execute only a Why Run ...${NC}\n"
+	why_run
+elif [[ $ARG == '--test' || $ARG == '-T' ]]
+then
+	printf "${GREEN}[INFO] Execute only tests ...${NC}\n"
+	exec_test
+else
+	printf "${GREEN}[INFO] Execute a Why Run ...${NC}\n"
+	why_run
+	if [[ $? == 0 ]]
+	then
+		updated_resources=`tail -1 $LOGFILE|awk '{print $4}'|cut -d '/' -f 1`
+		if [[ $updated_resources == 0 ]]
+		then
+			printf "\n${GREEN}[INFO] No resources to update, no need to converge.${NC}\n"
+		else
+			printf "\n${GREEN}[INFO] Why Run successfull, let's converge ...${NC}'\n"
+			chef_run
+			exec_test
+		fi
+	else
+		printf "\n${RED}[ERROR] Why Run encountered errors !${NC}\n"
+		exit 1;
+	fi
+fi
