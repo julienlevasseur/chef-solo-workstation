90e71b37958171a87cc6f638defe2f32bde49901
Julien Levasseur <levasju1@LAP-MTL-LEVASJU.vasco.com>
2018-01-26 15:47:20 -0500

    add metadata.rb

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name 'chef-solo_playground'
+maintainer 'Julien Levasseur'
+maintainer_email 'julien.levasseur@esignlive.com'
+license 'All Rights Reserved'
+description 'Installs/Configures workstation'
+long_description 'Installs/Configures workstation'
+version '0.1.0'
+chef_version '>= 12.1' if respond_to?(:chef_version)
